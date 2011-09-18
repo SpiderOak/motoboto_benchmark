4f1cf6e307a120a601b2c89032f85351e1e2d5dc
Doug Fort <dougfort@dougfort.com>
2011-09-18 15:01:16 -0400

    Initial stub: basic motoboto connection

---
+++ customer.py
@@ -0,0 +1,44 @@
+# -*- coding: utf-8 -*-
+"""
+customer.py
+
+A greenlet to represent a single nimbus.io customer
+"""
+import logging
+
+from  gevent.greenlet import Greenlet
+
+import motoboto
+from motoboto.config import config_template
+
+class Customer(Greenlet):
+    """
+    A greenlet object to represent a single nimbus.io customer
+    """
+    def __init__(self, halt_event, test_spec, pub_queue):
+        Greenlet.__init__(self)
+        self._log = logging.getLogger(test_spec["Username"])
+        self._halt_event = halt_event
+        self._test_spec = test_spec
+        self._pub_queue = pub_queue
+        self._s3_connection = None
+
+    def join(self, timeout=None):
+        """
+        close the _pull socket
+        """
+        if self._s3_connection is not None:
+            self._s3_connection.close()
+        Greenlet.join(self, timeout)
+
+    def _run(self):
+        config = config_template(
+            user_name=self._test_spec["Username"], 
+            auth_key_id=self._test_spec["AuthKeyId"], 
+            auth_key=self._test_spec["AuthKey"]
+        )
+        self._s3_connection = motoboto.connect_s3(config=config)
+
+        while not self._halt_event.is_set():
+            self._halt_event.wait(1.0)
+
---
+++ motoboto_benchmark_main.py
@@ -0,0 +1,143 @@
+# -*- coding: utf-8 -*-
+"""
+motoboto_benchmark_main.py
+
+A process that serves as a client to the SpiderOak lumberyard storage system
+Communication is through zeromq
+"""
+import json
+import logging
+import os
+import os.path
+import signal
+import sys
+
+import gevent
+from gevent import monkey
+monkey.patch_socket()
+
+import zmq
+from gevent.queue import Queue
+from gevent.event import Event
+
+from publisher import Publisher
+from customer import Customer
+
+_log_path = "motoboto_benchmark.log"
+_log_format_template = u'%(asctime)s %(levelname)-8s %(name)-20s: %(message)s'
+_pub_address = "ipc:///tmp/motoboto_benchmark-main-pub/socket"
+
+def _handle_sigterm(halt_event):
+    halt_event.set()
+
+def _prepare_ipc_path(address):
+    """
+    IPC sockets need an existing file for an address
+    """
+    path = address[len("ipc://"):]
+    dir_name = os.path.dirname(path)
+    if not os.path.exists(dir_name):
+        os.makedirs(dir_name)
+    if not os.path.exists(path):
+        with open(path, "w") as output_file:
+            output_file.write("pork")
+
+def _parse_command_line():
+    """Parse the command line, returning an options object"""
+    from optparse import OptionParser
+
+    parser = OptionParser()
+    parser.add_option(
+        '-l', "--log-path", dest="log_path", type="string",
+        help="full path of the log file"
+    )
+    parser.add_option(
+        '-t', "--test-dir", dest="test_dir", type="string",
+        help="path to a directory containing JSON test definition files"
+    )
+
+    parser.set_defaults(log_path=_log_path)
+
+    options, _ = parser.parse_args()
+
+    if options.test_dir is None:
+        print >> sys.stderr, "You must enter a test dir"
+        sys.exit(1)
+
+    return options
+
+def _initialize_logging(log_path):
+    """initialize the log"""
+    handler = logging.FileHandler(log_path, mode="a", encoding="utf-8")
+    formatter = logging.Formatter(_log_format_template)
+    handler.setFormatter(formatter)
+
+    # define a Handler which writes INFO messages or higher to the sys.stderr
+    console = logging.StreamHandler()
+    console.setLevel(logging.INFO)
+    formatter = logging.Formatter("%(levelname)-8s %(name)-20s %(message)s")
+    console.setFormatter(formatter)
+    
+    logging.root.addHandler(handler)
+    logging.root.addHandler(console)
+
+    logging.root.setLevel(logging.DEBUG)
+
+def main():
+    """
+    main processing module
+    """
+    options = _parse_command_line()
+    _initialize_logging(options.log_path)
+    log = logging.getLogger("main")
+    log.info("program starts")
+
+    halt_event = Event()
+    gevent.signal(signal.SIGTERM, _handle_sigterm, halt_event)
+
+    context = zmq.Context()
+    pub_queue = Queue()
+
+    _prepare_ipc_path(_pub_address)
+
+    publisher = Publisher(halt_event, context, _pub_address, pub_queue)
+
+    publisher.start()
+
+    log.info("loading test definition files from %r" % (options.test_dir, ))
+    customer_list = list()
+    for file_name in os.listdir(options.test_dir):
+        if file_name.endswith(".json"):
+            log.info("loading %r" % (file_name, ))
+            path = os.path.join(options.test_dir, file_name)
+            with open(path, "rt") as input_file:
+                test_spec = json.load(input_file)
+            customer = Customer(halt_event, test_spec, pub_queue)
+            customer.start()
+            customer_list.append(customer)
+
+    log.info("waiting")
+    try:
+        halt_event.wait()
+    except KeyboardInterrupt:
+        log.info("KeyBoardInterrupt")
+        halt_event.set()
+    
+    log.info("killing greenlets")
+    publisher.kill()
+    for customer in customer_list:
+        customer.kill()
+    
+    log.info("joining")
+    publisher.join()
+    for customer in customer_list:
+        customer.join()
+    
+    context.term()
+
+    log.info("program ends")
+    return 0
+
+if __name__ == "__main__":
+    sys.exit(main())
+
